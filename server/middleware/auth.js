025079e8de290222828a50936d13deb2070687ff
Somya Sonakshi <115647211+webCoder2303@users.noreply.github.com>
2025-07-20 22:18:05 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,7 @@
 
 import { jwt } from "jsonwebtoken";
 import User from "../models/User";
+import cloudinary from "../lib/cloudinary";
 
 export const protectRoute = async(req,res,next)=>{
     try {
@@ -21,4 +22,24 @@ export const protectRoute = async(req,res,next)=>{
 export const checkAuth = (req,res)=>{
     res.json({success:true,user:req.user});
     
+}
+
+
+export const updateProfile = async(req,res)=>{
+    try {
+        const {profilePic,bio,fullName} = req.body;
+        const userId = req.user._id
+        let updatedUser;
+        if(!profilePic){
+           updatedUser =  await User.findByIdAndUpdate(userId,{bio,fullName},{new:true})
+        }
+        else{
+            const upload = await cloudinary.uploader.upload(profilePic);
+            updatedUser = await User.findByIdAndUpdate(userId,{profilePic: upload.secure_url,bio,fullName},{new:true});
+        }
+        res.json({success:true,user:updatedUser})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false,message:error.message})
+    }
 }
