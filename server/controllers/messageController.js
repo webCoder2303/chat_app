a2d7e14b8a371360a30e733507c5cd44e8c9ed8a
Somya Sonakshi <115647211+webCoder2303@users.noreply.github.com>
2025-07-23 03:07:27 +0530

    Create messageController.js

---
+++ server/controllers/messageController.js
@@ -0,0 +1,60 @@
+//Get all users except the logged in user
+
+import Message from "../models/Message";
+import User from "../models/User";
+
+export const getUsersForSidebar = async()=>{
+    try {
+        const userId  = req.user._id;
+        const filteredUsers = await User.find({_id:{$ne:userId}})
+        .select("-password")
+        //Count number of messages not seen
+        const unseenMessages = {}
+        const promises = filteredUsers.map(async(user)=>{
+            const messages = await Message.find({senderId:user._id,
+                receiverId:userId,seen:false
+            })
+            if(messages.length>0){
+                unseenMessages[user._id] = messages.length;
+            }
+        })
+        await Promise.all(promises);
+        res.json({success:true,users:filteredUsers,unseenMessages})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false,message:error.message})
+    }
+}
+
+//Get all messages for selected user
+export const getMessages = async(req,res)=>{
+    try {
+        const {id:selectedUserId} = req.params;
+        const myId = req.user._id;
+        const messages = await  Message.find({
+            $or:[
+                {senderId:myId,receiverId:selectedUserId},
+                {senderId:selectedUserId,receiverId:myId}
+            ]
+        })
+        await Message.updateMany({senderId:selectedUserId,receiverId:myId},{seen:true});
+        res.json({success:true,messages})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false,message:error.message})
+    }
+}
+
+
+//api to mark message as seen using message id
+export const markMessageAsSeen = async(req,res)=>{
+    try {
+        const {id} = req.params;
+        await Message.findByIdAndUpdate(id,{seen:true})
+        res.json({success:true})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false,message:error.message})
+    }
+}
+
