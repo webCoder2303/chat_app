91d269c1ebd44abb7a076b44a25262cff1f05339
Somya Sonakshi <115647211+webCoder2303@users.noreply.github.com>
2025-07-23 02:34:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use("/api/auth",userRouter);
 //Connect to MongoDB
 await connectDB();
 
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log("Server is running on Port "+PORT)
 })
