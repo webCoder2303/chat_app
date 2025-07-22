cb1fcf760a6824ffca7c2fcf00101ee22840d21a
Somya Sonakshi <115647211+webCoder2303@users.noreply.github.com>
2025-07-23 02:12:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config"
 import cors from "cors"
 import http from "http"
 import { connectDB } from "./lib/db.js"
+import userRouter from "./routes/userRoute.js"
 
 dotenv.config()
 const app = express()
@@ -12,10 +13,9 @@ const server = http.createServer(app)
 app.use(express.json({limit:"4mb"}))
 app.use(cors())
 
-app.use("/api/status",(req,res)=>{
-    res.send("Server is running")
-})
-
+//Routes setup
+app.use("/api/status",(req,res)=>{ res.send("Server is running")})
+app.use("/api/auth",userRouter);
 
 //Connect to MongoDB
 await connectDB();
